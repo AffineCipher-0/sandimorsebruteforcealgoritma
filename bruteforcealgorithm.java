c492e4258f9c38e756c0a87eacf87cdfbcf5f0fb
Saka <gatotsasangkataka@gmail.com>
2023-03-08 01:52:50 +0700

    brute force java

---
+++ bruteforcealgorithm.java
@@ -0,0 +1,10 @@
+// Berfungsi untuk menemukan elemen maksimum dalam array menggunakan brute force
+int findMax(int arr[], int n) {
+    int maks = arr[0]; // Inisialisasi elemen maksimum ke elemen pertama array
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) { // Bandingkan elemen saat ini dengan elemen maksimum
+            maks = arr[i]; // Perbarui elemen maksimum jika elemen saat ini lebih besar
+        }
+    }
+    pengembalian maks; // Mengembalikan elemen maksimum
+}
